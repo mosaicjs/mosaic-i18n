43a3e0ea91e4567180dd181f980fbeaab0dd61e8
mkotelnikov <mikhail.kotelnikov@ubimix.com>
2015-06-21 19:45:42 +0200

    Implement internationalization management

--- dist/index.js
+++ dist/index.js
@@ -66,8 +66,13 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	var _libI18N2 = _interopRequireDefault(_libI18N);
 
+	var _libTextTemplate = __webpack_require__(2);
+
+	var _libTextTemplate2 = _interopRequireDefault(_libTextTemplate);
+
 	exports['default'] = {
-	    I18N: _libI18N2['default']
+	    I18N: _libI18N2['default'],
+	    TextTemplate: _libTextTemplate2['default']
 	};
 	module.exports = exports['default'];
 
@@ -75,20 +80,161 @@ return /******/ (function(modules) { // webpackBootstrap
 /* 1 */
 /***/ function(module, exports) {
 
-	"use strict";
+	'use strict';
 
-	Object.defineProperty(exports, "__esModule", {
-	  value: true
+	Object.defineProperty(exports, '__esModule', {
+	    value: true
 	});
 
-	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+	var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
-	var I18N = function I18N() {
-	  _classCallCheck(this, I18N);
-	};
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
 
-	exports["default"] = I18N;
-	module.exports = exports["default"];
+	var I18N = (function () {
+	    function I18N() {
+	        _classCallCheck(this, I18N);
+
+	        this._locales = {};
+	    }
+
+	    _createClass(I18N, [{
+	        key: 'registerTranslations',
+
+	        /**
+	         * Registers translations for the specified locale and batch.
+	         * 
+	         * @param locale
+	         *            the name of the locale
+	         * @param batchKey
+	         *            the key of the message batch
+	         * @param translation
+	         *            an object containing message keys and the corresponding
+	         *            translations
+	         */
+	        value: function registerTranslations(locale, batchKey, translations) {
+	            var localeIndex = this._locales[locale] = this._locales[locale] || {};
+	            localeIndex[batchKey] = translations;
+	        }
+	    }, {
+	        key: 'unregisterTranslations',
+
+	        /**
+	         * Unregisters translations for the specified locale and batch.
+	         * 
+	         * @param locale
+	         *            the name of the locale
+	         * @param batchKey
+	         *            the key of the message batch
+	         */
+	        value: function unregisterTranslations(locale, batchKey) {
+	            var localeIndex = this._locales[locale];
+	            if (localeIndex) {
+	                delete localeIndex[batchKey];
+	            }
+	        }
+	    }, {
+	        key: 'newMessages',
+
+	        /**
+	         * Returns a translated batch corresponding to the specified batch key.
+	         * 
+	         * @param locale
+	         *            the locale of the required translations
+	         * @param batchKey
+	         *            key of the batch
+	         * @param batch
+	         *            an optional batch of default messages and utility methods
+	         */
+	        value: function newMessages(locale, batchKey, batch) {
+	            batch = batch || {};
+	            var localeIndex = this._locales[locale] || {};
+	            var translations = localeIndex[batchKey] || {};
+	            var result = {};
+	            var excluded = {};
+	            for (var key in batch) {
+	                var val = batch[key];
+	                var newVal = translations[key];
+	                if (typeof val === 'function' && !!newVal && typeof newVal !== 'function') {
+	                    newVal = this._getTemplateFunction(newVal);
+	                }
+	                result[key] = newVal || val;
+	                excluded[key] = true;
+	            }
+	            for (var key in translations) {
+	                if (key in excluded) continue;
+	                result[key] = translations[key];
+	            }
+	            return result;
+	        }
+	    }, {
+	        key: '_getTemplateFunction',
+	        value: function _getTemplateFunction(val) {
+	            var templ = TextTemplate.compile('' + message);
+	            return function () {
+	                return templ.apply(templ, arguments);
+	            };
+	        }
+	    }]);
+
+	    return I18N;
+	})();
+
+	exports['default'] = I18N;
+	module.exports = exports['default'];
+
+/***/ },
+/* 2 */
+/***/ function(module, exports) {
+
+	/**
+	 * A very simple (simplistic) library replacing variables in strings.
+	 */
+	'use strict';
+
+	Object.defineProperty(exports, '__esModule', {
+	    value: true
+	});
+
+	var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+	var TextTemplate = (function () {
+	    function TextTemplate() {
+	        _classCallCheck(this, TextTemplate);
+	    }
+
+	    _createClass(TextTemplate, null, [{
+	        key: 'compile',
+	        value: function compile(text) {
+	            text = text || '';
+	            var array = text.split(/\$\{|\}/);
+	            for (var i = 0; i < array.length; i++) {
+	                if (i % 2 === 1) {
+	                    array[i] = array[i].replace(/^\s+|\s+$/, '');
+	                }
+	            }
+	            return function (obj) {
+	                var str = '';
+	                for (var i = 0; i < array.length; i++) {
+	                    if (i % 2 === 0) {
+	                        str += array[i];
+	                    } else {
+	                        var key = array[i];
+	                        var val = obj[key] || '';
+	                        str += val;
+	                    }
+	                }
+	                return str;
+	            };
+	        }
+	    }]);
+
+	    return TextTemplate;
+	})();
+
+	exports['default'] = TextTemplate;
+	module.exports = exports['default'];
 
 /***/ }
 /******/ ])
--- test/I18NTest.js
+++ test/I18NTest.js
@@ -2,7 +2,159 @@ import expect from 'expect.js';
 import { I18N } from '../';
 
 describe('I18N', function(){
-    it('should return default values', function() {
+    
+    describe('compileTextTemplate', function(){
+        it('should return simple strings without modifications', function() {
+            let templ = I18N.compileTextTemplate('abc');
+            expect(templ({})).to.be('abc');
+        });
+        it('should replace simple variables', function() {
+            let templ = I18N.compileTextTemplate('Hello, ${name}!');
+            let str = templ({ name : 'World' });
+            expect(str).to.be('Hello, World!');
+        });
+        it('should replace multiple variables (1)', function() {
+            let templ = I18N.compileTextTemplate('abc ${x} cde ${y} efg ${z} ghi');
+            let str = templ({ 
+                x : 'X',
+                y : 'Y',
+                z : 'Z'
+            });
+            expect(str).to.be('abc X cde Y efg Z ghi');
+        });
+        it('should replace multiple variables (2)', function() {
+            let templ = I18N.compileTextTemplate('abc \n ${  x  } cde \n${ y } efg \n${ \n  z \n} \nghi');
+            let str = templ({ 
+                x : 'X',
+                y : 'Y',
+                z : 'Z'
+            });
+            expect(str).to.be('abc \n X cde \nY efg \nZ \nghi');
+        });
+    });
+    
+    describe('newMessage should provide tranlations', function(){
+        let i18n = new I18N();
+        let batchKey = 'my.messages';
+        let batch = {
+            first : 'Hello',
+            second : 'World',
+            message : function(prefix){
+                return prefix + ': ' + this.first + ', ' + this.second + '!';
+            }
+        };
+        
+        i18n.registerTranslations('fr', batchKey, {
+            first: 'Bonjour',
+            second : 'Le Monde',
+            third : 'Blah-blah'
+        });
+        it('should return default values for non-registred locales', function() {
+            let messages = i18n.newMessages('non-existing-locale', batchKey, batch);
+            expect(!!messages).to.be(true);
+            expect(!!messages.first).to.be(true);
+            expect(!!messages.second).to.be(true);
+            expect(!!messages.message).to.be(true);
+            expect(messages.first).to.eql('Hello');
+            expect(messages.second).to.eql('World');
+            expect(messages.third).to.be(undefined);
+            expect(messages.message('xxx')).to.eql('xxx: Hello, World!');
+        });
+        it('should return translated values for registred locales', function() {
+            let messages = i18n.newMessages('fr', batchKey, batch);
+            expect(!!messages).to.be(true);
+            expect(!!messages.first).to.be(true);
+            expect(!!messages.second).to.be(true);
+            expect(!!messages.message).to.be(true);
+            expect(messages.first).to.eql('Bonjour');
+            expect(messages.second).to.eql('Le Monde');
+            expect(messages.third).to.eql('Blah-blah');
+            expect(messages.message('xxx')).to.eql('xxx: Bonjour, Le Monde!');
+        });
         
     });
+    
+    describe('newMessage should generate template functions', function(){
+        let i18n = new I18N();
+        let batchKey = 'my.messages';
+        let batch = { message : function(params){
+            return 'default value';
+        } };
+        
+        i18n.registerTranslations('fr', batchKey, {
+            message: 'Bonjour ${prefix} ${firstName} ${lastName} !'
+        });
+        i18n.registerTranslations('en', batchKey, {
+            message: 'Hello ${prefix} ${firstName} ${lastName}!'
+        });
+
+        it('should return empty string for non-existing translation keys', function() {
+            let messages = i18n.newMessages('non-existing-locale', batchKey, batch);
+            expect(!!messages).to.be(true);
+            expect(!!messages.message).to.be(true);
+            let str = messages.message({
+                prefix: 'Mr',
+                firstName : 'John',
+                lastName : 'Doe'
+            });
+            expect(str).to.eql('default value');
+        });
+
+        it('en: should transform string templates to functions', function() {
+            let messages = i18n.newMessages('en', batchKey, batch);
+            expect(!!messages).to.be(true);
+            expect(!!messages.message).to.be(true);
+            let str = messages.message({
+                prefix: 'Mr',
+                firstName : 'John',
+                lastName : 'Doe'
+            });
+            expect(str).to.eql('Hello Mr John Doe!');
+        });
+        it('fr: should transform string templates to functions', function() {
+            let messages = i18n.newMessages('fr', batchKey, batch);
+            expect(!!messages).to.be(true);
+            expect(!!messages.message).to.be(true);
+            let str = messages.message({
+                prefix: 'Mr',
+                firstName : 'John',
+                lastName : 'Doe'
+            });
+            expect(str).to.eql('Bonjour Mr John Doe !');
+        });
+    });
+    
+    describe('getMessage should return alwasy the same batch', function(){
+        let i18n = new I18N();
+        let batchKey = 'my.messages';
+        let batch = {
+            first : 'Hello',
+            second : 'World',
+        };
+        i18n.registerTranslations('fr', batchKey, {
+            first : 'Bonjour',
+            second : 'Le Monde',
+        });
+
+        it('should multiple times the same object', function() {
+            let messages = i18n.getMessages('fr', batchKey, batch);
+            expect(!!messages).to.be(true);
+            expect(messages.first).to.eql('Bonjour');
+            expect(messages.second).to.eql('Le Monde');
+            expect(!!messages.id).to.be(false);
+            messages.id = '123';
+            let test = i18n.getMessages('fr', batchKey, batch);
+            expect(test).to.be(messages);
+            expect(test.id).to.eql('123');
+            
+            i18n.clearCache();
+            test = i18n.getMessages('fr', batchKey, batch);
+            expect(test).to.not.be(messages);
+            expect(!!test).to.be(true);
+            expect(test.id).to.be(undefined);
+            expect(test.first).to.eql('Bonjour');
+            expect(test.second).to.eql('Le Monde');
+        });        
+    });
+    
 });
